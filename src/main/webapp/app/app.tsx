a4ea13ecb94f0e958318e8c8bd36a465cd66115c
Victor Fu <victor@dentall.io>
2019-01-05 16:22:56 +0000

    Back to origin web style

--- src/main/webapp/app/app.tsx
+++ src/main/webapp/app/app.tsx
@@ -3,10 +3,9 @@ import './app.css';
 
 import React from 'react';
 import { connect } from 'react-redux';
-import { Card, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Card } from 'reactstrap';
 import { HashRouter as Router } from 'react-router-dom';
 import { ToastContainer, ToastPosition, toast } from 'react-toastify';
-import BurgerMenu from 'react-burger-menu';
 
 import { IRootState } from 'app/shared/reducers';
 import { getSession } from 'app/shared/reducers/authentication';
@@ -14,7 +13,6 @@ import { getProfile } from 'app/shared/reducers/application-profile';
 import { setLocale } from 'app/shared/reducers/locale';
 import Header from 'app/shared/layout/header/header';
 import Footer from 'app/shared/layout/footer/footer';
-import Sidebar from 'app/shared/layout/sidebar/sidebar';
 import { hasAnyAuthority } from 'app/shared/auth/private-route';
 import ErrorBoundary from 'app/shared/error/error-boundary';
 import { AUTHORITIES } from 'app/config/constants';
@@ -22,35 +20,24 @@ import AppRoutes from 'app/routes';
 
 export interface IAppProps extends StateProps, DispatchProps {}
 
-interface IAppState {
-  isOpen?: boolean;
-}
-
-export class App extends React.Component<IAppProps, IAppState> {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isOpen: false
-    };
-  }
-
+export class App extends React.Component<IAppProps> {
   componentDidMount() {
     this.props.getSession();
     this.props.getProfile();
   }
 
-  isMenuOpen = state => this.setState({ isOpen: state.isOpen });
-
   render() {
     const paddingTop = '60px';
-    const Menu = BurgerMenu['push'];
-    const { isOpen } = this.state;
     return (
       <Router>
-        <div id="outer-container" style={{ height: '100%' }}>
-          <Menu id={'push'} pageWrapId={'page-wrap'} outerContainerId={'outer-container'} isOpen={isOpen} onStateChange={this.isMenuOpen}>
-            <Sidebar
+        <div className="app-container" style={{ paddingTop }}>
+          <ToastContainer
+            position={toast.POSITION.TOP_LEFT as ToastPosition}
+            className="toastify-container"
+            toastClassName="toastify-toast"
+          />
+          <ErrorBoundary>
+            <Header
               isAuthenticated={this.props.isAuthenticated}
               isAdmin={this.props.isAdmin}
               currentLocale={this.props.currentLocale}
@@ -59,36 +46,15 @@ export class App extends React.Component<IAppProps, IAppState> {
               isInProduction={this.props.isInProduction}
               isSwaggerEnabled={this.props.isSwaggerEnabled}
             />
-          </Menu>
-          <main id="page-wrap">
-            <div className="app-container" style={{ paddingTop }}>
-              <ToastContainer
-                position={toast.POSITION.TOP_LEFT as ToastPosition}
-                className="toastify-container"
-                toastClassName="toastify-toast"
-              />
+          </ErrorBoundary>
+          <div className="container-fluid view-container" id="app-view-container">
+            <Card className="jh-card">
               <ErrorBoundary>
-                <Header
-                  isAuthenticated={this.props.isAuthenticated}
-                  isAdmin={this.props.isAdmin}
-                  currentLocale={this.props.currentLocale}
-                  onLocaleChange={this.props.setLocale}
-                  ribbonEnv={this.props.ribbonEnv}
-                  isInProduction={this.props.isInProduction}
-                  isSwaggerEnabled={this.props.isSwaggerEnabled}
-                  isMenuOpen={this.state.isOpen}
-                />
+                <AppRoutes />
               </ErrorBoundary>
-              <div className="container-fluid view-container" id="app-view-container">
-                <Card className="jh-card">
-                  <ErrorBoundary>
-                    <AppRoutes />
-                  </ErrorBoundary>
-                </Card>
-                <Footer />
-              </div>
-            </div>
-          </main>
+            </Card>
+            <Footer />
+          </div>
         </div>
       </Router>
     );
