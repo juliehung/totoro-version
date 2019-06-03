bbaedad8c2f709ea12c74a7260958d8c7f433169
Jim Lin <acgsong.tw@yahoo.com.tw>
2019-06-03 07:17:13 +0000

    Resolve "Add examination code and point"

--- src/main/java/io/dentall/totoro/service/NhiExtendDisposalService.java
+++ src/main/java/io/dentall/totoro/service/NhiExtendDisposalService.java
@@ -113,91 +113,99 @@ public class NhiExtendDisposalService {
             .findById(updateNhiExtendDisposal.getId())
             .map(nhiExtendDisposal -> {
                 if (updateNhiExtendDisposal.getA11() != null) {
-                    nhiExtendDisposal.setA11((updateNhiExtendDisposal.getA11()));
+                    nhiExtendDisposal.setA11(updateNhiExtendDisposal.getA11());
                 }
 
                 if (updateNhiExtendDisposal.getA12() != null) {
-                    nhiExtendDisposal.setA12((updateNhiExtendDisposal.getA12()));
+                    nhiExtendDisposal.setA12(updateNhiExtendDisposal.getA12());
                 }
 
                 if (updateNhiExtendDisposal.getA13() != null) {
-                    nhiExtendDisposal.setA13((updateNhiExtendDisposal.getA13()));
+                    nhiExtendDisposal.setA13(updateNhiExtendDisposal.getA13());
                 }
 
                 if (updateNhiExtendDisposal.getA14() != null) {
-                    nhiExtendDisposal.setA14((updateNhiExtendDisposal.getA14()));
+                    nhiExtendDisposal.setA14(updateNhiExtendDisposal.getA14());
                 }
 
                 if (updateNhiExtendDisposal.getA15() != null) {
-                    nhiExtendDisposal.setA15((updateNhiExtendDisposal.getA15()));
+                    nhiExtendDisposal.setA15(updateNhiExtendDisposal.getA15());
                 }
 
                 if (updateNhiExtendDisposal.getA16() != null) {
-                    nhiExtendDisposal.setA16((updateNhiExtendDisposal.getA16()));
+                    nhiExtendDisposal.setA16(updateNhiExtendDisposal.getA16());
                 }
 
                 if (updateNhiExtendDisposal.getA17() != null) {
-                    nhiExtendDisposal.setA17((updateNhiExtendDisposal.getA17()));
+                    nhiExtendDisposal.setA17(updateNhiExtendDisposal.getA17());
                 }
 
                 if (updateNhiExtendDisposal.getA18() != null) {
-                    nhiExtendDisposal.setA18((updateNhiExtendDisposal.getA18()));
+                    nhiExtendDisposal.setA18(updateNhiExtendDisposal.getA18());
                 }
 
                 if (updateNhiExtendDisposal.getA19() != null) {
-                    nhiExtendDisposal.setA19((updateNhiExtendDisposal.getA19()));
+                    nhiExtendDisposal.setA19(updateNhiExtendDisposal.getA19());
                 }
 
                 if (updateNhiExtendDisposal.getA22() != null) {
-                    nhiExtendDisposal.setA22((updateNhiExtendDisposal.getA22()));
+                    nhiExtendDisposal.setA22(updateNhiExtendDisposal.getA22());
                 }
 
                 if (updateNhiExtendDisposal.getA23() != null) {
-                    nhiExtendDisposal.setA23((updateNhiExtendDisposal.getA23()));
+                    nhiExtendDisposal.setA23(updateNhiExtendDisposal.getA23());
                 }
 
                 if (updateNhiExtendDisposal.getA25() != null) {
-                    nhiExtendDisposal.setA25((updateNhiExtendDisposal.getA25()));
+                    nhiExtendDisposal.setA25(updateNhiExtendDisposal.getA25());
                 }
 
                 if (updateNhiExtendDisposal.getA26() != null) {
-                    nhiExtendDisposal.setA26((updateNhiExtendDisposal.getA26()));
+                    nhiExtendDisposal.setA26(updateNhiExtendDisposal.getA26());
                 }
 
                 if (updateNhiExtendDisposal.getA27() != null) {
-                    nhiExtendDisposal.setA27((updateNhiExtendDisposal.getA27()));
+                    nhiExtendDisposal.setA27(updateNhiExtendDisposal.getA27());
                 }
 
                 if (updateNhiExtendDisposal.getA31() != null) {
-                    nhiExtendDisposal.setA31((updateNhiExtendDisposal.getA31()));
+                    nhiExtendDisposal.setA31(updateNhiExtendDisposal.getA31());
                 }
 
                 if (updateNhiExtendDisposal.getA32() != null) {
-                    nhiExtendDisposal.setA32((updateNhiExtendDisposal.getA32()));
+                    nhiExtendDisposal.setA32(updateNhiExtendDisposal.getA32());
                 }
 
                 if (updateNhiExtendDisposal.getA41() != null) {
-                    nhiExtendDisposal.setA41((updateNhiExtendDisposal.getA41()));
+                    nhiExtendDisposal.setA41(updateNhiExtendDisposal.getA41());
                 }
 
                 if (updateNhiExtendDisposal.getA42() != null) {
-                    nhiExtendDisposal.setA42((updateNhiExtendDisposal.getA42()));
+                    nhiExtendDisposal.setA42(updateNhiExtendDisposal.getA42());
                 }
 
                 if (updateNhiExtendDisposal.getA43() != null) {
-                    nhiExtendDisposal.setA43((updateNhiExtendDisposal.getA43()));
+                    nhiExtendDisposal.setA43(updateNhiExtendDisposal.getA43());
                 }
 
                 if (updateNhiExtendDisposal.getA44() != null) {
-                    nhiExtendDisposal.setA44((updateNhiExtendDisposal.getA44()));
+                    nhiExtendDisposal.setA44(updateNhiExtendDisposal.getA44());
                 }
 
                 if (updateNhiExtendDisposal.getA54() != null) {
-                    nhiExtendDisposal.setA54((updateNhiExtendDisposal.getA54()));
+                    nhiExtendDisposal.setA54(updateNhiExtendDisposal.getA54());
                 }
 
                 if (updateNhiExtendDisposal.getUploadStatus() != null) {
                     nhiExtendDisposal.setUploadStatus(updateNhiExtendDisposal.getUploadStatus());
+                }
+
+                if (updateNhiExtendDisposal.getExaminationCode() != null) {
+                    nhiExtendDisposal.setExaminationCode(updateNhiExtendDisposal.getExaminationCode());
+                }
+
+                if (updateNhiExtendDisposal.getExaminationPoint() != null) {
+                    nhiExtendDisposal.setExaminationPoint(updateNhiExtendDisposal.getExaminationPoint());
                 }
 
                 log.debug("Update nhiExtendTreatmentProcedures({}) of NhiExtendDisposal(id: {})", updateNhiExtendDisposal.getNhiExtendTreatmentProcedures(), updateNhiExtendDisposal.getId());
