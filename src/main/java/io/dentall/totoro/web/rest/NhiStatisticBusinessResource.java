9b6c1fb4faf4dc1e2c5d1ad77b2cc02143d1f8f8
黃偉程 <weichen@dentall.io>
2019-09-23 14:58:10 +0000

    Resolve "[Statistic] Expose dashboard resource"

--- src/main/java/io/dentall/totoro/web/rest/NhiStatisticBusinessResource.java
+++ src/main/java/io/dentall/totoro/web/rest/NhiStatisticBusinessResource.java
@@ -2,12 +2,20 @@ package io.dentall.totoro.web.rest;
 
 import com.codahale.metrics.annotation.Timed;
 import io.dentall.totoro.business.service.nhi.NhiAbnormalityService;
+import io.dentall.totoro.business.service.nhi.NhiStatisticService;
 import io.dentall.totoro.business.vm.nhi.NhiAbnormality;
+import io.dentall.totoro.business.vm.nhi.NhiStatisticDashboard;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.YearMonth;
+import java.util.List;
 
 /**
  *
@@ -20,8 +28,14 @@ public class NhiStatisticBusinessResource {
 
     private final NhiAbnormalityService nhiAbnormalityService;
 
-    public NhiStatisticBusinessResource(NhiAbnormalityService nhiAbnormalityService) {
+    private final NhiStatisticService nhiStatisticService;
+
+    public NhiStatisticBusinessResource(
+        NhiAbnormalityService nhiAbnormalityService,
+        NhiStatisticService nhiStatisticService
+    ) {
         this.nhiAbnormalityService = nhiAbnormalityService;
+        this.nhiStatisticService = nhiStatisticService;
     }
 
     @GetMapping("/abnormality")
@@ -29,5 +43,12 @@ public class NhiStatisticBusinessResource {
     public ResponseEntity<NhiAbnormality> getNhiAbnormalityDoctors(@RequestParam int yyyymm) {
         log.debug("REST request to get NhiAbnormality by year month: {}", yyyymm);
         return new ResponseEntity<>(nhiAbnormalityService.getNhiAbnormality(yyyymm), HttpStatus.OK);
+    }
+
+    @GetMapping("/dashboard")
+    @Timed
+    public ResponseEntity<List<NhiStatisticDashboard>> getDashboard(@RequestParam int yyyymm) {
+        log.debug("REST request to get Dashboard by year month: {}", yyyymm);
+        return new ResponseEntity<>(nhiStatisticService.calculate(YearMonth.of(yyyymm / 100, yyyymm % 100)), HttpStatus.OK);
     }
 }
