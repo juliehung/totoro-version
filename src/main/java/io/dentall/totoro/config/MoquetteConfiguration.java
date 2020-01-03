fab8519ed6eefca8f11edc8c1158610742ac9b81
Jim Lin <acgsong.tw@yahoo.com.tw>
2020-01-03 11:29:59 +0000

    Resolve "Expose moquette porperties"

--- src/main/java/io/dentall/totoro/config/MoquetteConfiguration.java
+++ src/main/java/io/dentall/totoro/config/MoquetteConfiguration.java
@@ -1,18 +1,18 @@
 package io.dentall.totoro.config;
 
 import io.github.jhipster.config.JHipsterConstants;
+import io.moquette.BrokerConstants;
 import io.moquette.broker.Server;
-import io.moquette.broker.config.ClasspathResourceLoader;
-import io.moquette.broker.config.IConfig;
-import io.moquette.broker.config.IResourceLoader;
-import io.moquette.broker.config.ResourceLoaderConfig;
+import io.moquette.broker.config.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import java.io.IOException;
+import java.util.Properties;
 
 @Profile("!" + JHipsterConstants.SPRING_PROFILE_TEST)
 @Configuration
@@ -20,13 +20,26 @@ public class MoquetteConfiguration {
 
     private final Logger log = LoggerFactory.getLogger(MoquetteConfiguration.class);
 
+    @Value("${moquette.port:1883}")
+    private int port;
+
+    @Value("${moquette.host:0.0.0.0}")
+    private String host;
+
+    @Value("${moquette.websocket_port:8081}")
+    private int websocketPort;
+
     @Bean(destroyMethod = "stopServer")
     public Server embeddedMoquetteBrokerServer() throws IOException {
-        IResourceLoader classpathLoader = new ClasspathResourceLoader();
-        IConfig classPathConfig = new ResourceLoaderConfig(classpathLoader);
+        MemoryConfig config = new MemoryConfig(new Properties());
+        config.setProperty("port", Integer.toString(port));
+        config.setProperty("host", host);
+        config.setProperty("websocket_port", Integer.toString(websocketPort));
+        config.setProperty(BrokerConstants.ALLOW_ANONYMOUS_PROPERTY_NAME, "true");
+
         Server mqttBroker = new Server();
-        mqttBroker.startServer(classPathConfig);
-        log.info("Starting MQTT on host: {} and port: {}", classPathConfig.getProperty("host"), classPathConfig.getProperty("port"));
+        mqttBroker.startServer(config);
+        log.info("Starting MQTT on host: {} and port: {}", host, port);
 
         return mqttBroker;
     }
