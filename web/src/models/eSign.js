edbf69e2f17aa9293f80ac34393df7290a83754b
jinbin xu <xu@dentall.io>
2020-01-21 10:01:55 +0000

    add canvas background & sourceType for api

--- web/src/models/eSign.js
+++ web/src/models/eSign.js
@@ -1,20 +1,20 @@
 import request from '../utils/request';
 import apiUrl from '../utils/apiUrl';
 
-const LOCATION = `esigns`;
+const LOCATION = `business/esigns`;
 const requestUrl = `${apiUrl}/${LOCATION}`;
 
 export default class ESign {
   // GET by id
   static getById = async id => {
-    let requestURL = `${requestUrl}/${id}`;
+    let requestURL = `${requestUrl}?id.equals=${id}`;
     const result = await request(requestURL);
     return result;
   };
 
   // POST
   static create = async esign => {
-    let requestURL = `${requestUrl}`;
+    let requestURL = `${requestUrl}/string64`;
     const options = {
       headers: {
         'content-type': 'application/json',
