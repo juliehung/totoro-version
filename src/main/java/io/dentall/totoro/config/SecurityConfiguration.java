0a9913235a9a1b4ee571a36de534d93dc9fbdaaa
weichen1 <weichen@dentall.io>
2020-03-06 15:17:17 +0800

    Add api as permit

--- src/main/java/io/dentall/totoro/config/SecurityConfiguration.java
+++ src/main/java/io/dentall/totoro/config/SecurityConfiguration.java
@@ -108,6 +108,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/api/account/reset-password/init").permitAll()
             .antMatchers("/api/account/reset-password/finish").permitAll()
             .antMatchers("/api/greeting").permitAll()
+            .antMatchers("/api/version").permitAll()
             .antMatchers("/api/images/test").permitAll()
             .antMatchers("/api/images/host").permitAll()
             .antMatchers("/api/**").authenticated()
