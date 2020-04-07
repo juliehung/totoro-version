900debedd717525797988669259f3c0e7c60acb6
xu <xu@dentall.io>
2020-04-07 17:42:28 +0800

    remove ()

--- web/src/container/AppointmentPage/utils/filterDisposalsToSortedTodos.js
+++ web/src/container/AppointmentPage/utils/filterDisposalsToSortedTodos.js
@@ -3,7 +3,7 @@ import { DisposalStatus } from '../constant';
 
 export function filterDisposalsToSortedTodos(disposals) {
   return disposals
-    .filter((d) => d.status === DisposalStatus.PERMANENT)
-    .filter((d) => d.todo)
+    .filter(d => d.status === DisposalStatus.PERMANENT)
+    .filter(d => d.todo)
     .sort((a, b) => moment(a.todo.createdDate).diff(moment(b.todo.createdDate)));
 }
