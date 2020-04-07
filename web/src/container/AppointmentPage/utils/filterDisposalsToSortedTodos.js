bc0169bb4dc4a2485a2684262c9458162e1414da
xu <xu@dentall.io>
2020-04-07 17:38:04 +0800

    fix disposal does not contain todo

--- web/src/container/AppointmentPage/utils/filterDisposalsToSortedTodos.js
+++ web/src/container/AppointmentPage/utils/filterDisposalsToSortedTodos.js
@@ -3,6 +3,7 @@ import { DisposalStatus } from '../constant';
 
 export function filterDisposalsToSortedTodos(disposals) {
   return disposals
+    .filter(d => d.todo)
     .filter(d => d.status === DisposalStatus.PERMANENT)
     .sort((a, b) => moment(a.todo.createdDate).diff(moment(b.todo.createdDate)));
 }
