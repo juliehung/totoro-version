e2d9a71c19bbaaf0c928494d1d2e5628b751b26f
jinbin xu <xu@dentall.io>
2020-05-08 01:44:35 +0000

    Jim t

--- web/src/container/RegistrationPage/index.js
+++ web/src/container/RegistrationPage/index.js
@@ -35,8 +35,21 @@ const DatePickerContainer = styled.div`
 `;
 const NameContainer = styled.div`
   display: flex;
-  width: 200px;
   align-items: center;
+  & > div {
+    flex: 1 1;
+    width: 5px;
+    & > * {
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  }
+  @media (max-width: 768px) {
+    & img {
+      display: none;
+    }
+  }
 `;
 const StyledTable = styled(Table)`
   flex-grow: 1;
@@ -72,6 +85,77 @@ const AvatarImg = styled.img`
   margin-right: 10px;
   flex-shrink: 0;
 `;
+
+const renderAvatarImg = gender => {
+  if (gender === 'MALE') return <AvatarImg src={ManPng} alt="male" />;
+  if (gender === 'FEMALE') return <AvatarImg src={WomanPng} alt="female" />;
+  return <AvatarImg src={DefaultPng} alt="default" />;
+};
+
+const renderName = patient => {
+  return (
+    <Tooltip title={patient.name}>
+      <NameContainer>
+        {renderAvatarImg(patient.gender)}
+        <div>
+          <span>{patient.medicalId}</span>
+          <Title level={4}>{patient.name}</Title>
+        </div>
+      </NameContainer>
+    </Tooltip>
+  );
+};
+
+const renderRowIndex = (rowIndex, status) => {
+  let c = Gray700;
+  if (status === 'IN_PROGRESS') c = G1;
+  if (status === 'PENDING') c = B1;
+  return (
+    <RowIndexContainer>
+      <Status style={{ background: c }} />
+      <StyledTitle level={3}>{rowIndex}</StyledTitle>
+    </RowIndexContainer>
+  );
+};
+
+const renderSubject = (subject, note) => {
+  if ((!subject || subject === '') && (!note || note === '')) {
+    return '無';
+  }
+  return subject && subject !== '' ? subject + ',' + note : note;
+};
+
+const convertToTableSource = (registrations, selectedDoctor) => {
+  const tableSource = registrations
+    // Don't show cancel appointment and no registration
+    .filter(appt => appt.status !== 'CANCEL' && appt.registration && appt.registration.id)
+    .filter(appt => {
+      if (!selectedDoctor || !appt.doctor.user || selectedDoctor === allDoctors) {
+        return true;
+      }
+      return appt.doctor.user.login === selectedDoctor;
+    })
+    .slice()
+    .sort((a, b) => moment(a.registration.arrivalTime).unix() - moment(b.registration.arrivalTime).unix());
+
+  let i = 1;
+  return tableSource.map(appt => {
+    return {
+      key: appt.id,
+      rowIndex: renderRowIndex(i++, appt.registration.status),
+      name: renderName(appt.patient),
+      patient: appt.patient,
+      arrivalTime:
+        appt.registration.arrivalTime && moment(appt.registration.arrivalTime).local().format('YYYY-MM-DD HH:mm'),
+      expectedArrivalTime:
+        appt.expectedArrivalTime && moment(appt.expectedArrivalTime).local().format('YYYY-MM-DD HH:mm'),
+      age: moment().diff(appt.patient.birth, 'years') + 'Y',
+      type: appt.registration.type,
+      doctor: appt.doctor.user.firstName,
+      subject: renderSubject(appt.subject, appt.note),
+    };
+  });
+};
 
 const columns = [
   {
@@ -181,38 +265,6 @@ function RegistrationPage(props) {
     getRegistrations(start, end);
   };
 
-  const renderRowIndex = (rowIndex, status) => {
-    let c = Gray700;
-    if (status === 'IN_PROGRESS') c = G1;
-    if (status === 'PENDING') c = B1;
-    return (
-      <RowIndexContainer>
-        <Status style={{ background: c }} />
-        <StyledTitle level={3}>{rowIndex}</StyledTitle>
-      </RowIndexContainer>
-    );
-  };
-
-  const renderAvatarImg = gender => {
-    if (gender === 'MALE') return <AvatarImg src={ManPng} alt="male" />;
-    if (gender === 'FEMALE') return <AvatarImg src={WomanPng} alt="female" />;
-    return <AvatarImg src={DefaultPng} alt="default" />;
-  };
-
-  const renderName = patient => {
-    return (
-      <Tooltip title={patient.name}>
-        <NameContainer>
-          {renderAvatarImg(patient.gender)}
-          <div>
-            <span>{patient.medicalId}</span>
-            <Title level={4}>{patient.name}</Title>
-          </div>
-        </NameContainer>
-      </Tooltip>
-    );
-  };
-
   const renderDoctorSelect = () => {
     if (props.doctors) {
       const options = props.doctors.map(doctor => {
@@ -233,45 +285,6 @@ function RegistrationPage(props) {
         </Select>
       );
     }
-  };
-
-  const renderSubject = (subject, note) => {
-    if ((!subject || subject === '') && (!note || note === '')) {
-      return '無';
-    }
-    return subject && subject !== '' ? subject + ',' + note : note;
-  };
-
-  const convertToTableSource = registrations => {
-    const tableSource = registrations
-      // Don't show cancel appointment and no registration
-      .filter(appt => appt.status !== 'CANCEL' && appt.registration && appt.registration.id)
-      .filter(appt => {
-        if (!selectedDoctor || !appt.doctor.user || selectedDoctor === allDoctors) {
-          return true;
-        }
-        return appt.doctor.user.login === selectedDoctor;
-      })
-      .slice()
-      .sort((a, b) => moment(a.registration.arrivalTime).unix() - moment(b.registration.arrivalTime).unix());
-
-    let i = 1;
-    return tableSource.map(appt => {
-      return {
-        key: appt.id,
-        rowIndex: renderRowIndex(i++, appt.registration.status),
-        name: renderName(appt.patient),
-        patient: appt.patient,
-        arrivalTime:
-          appt.registration.arrivalTime && moment(appt.registration.arrivalTime).local().format('YYYY-MM-DD HH:mm'),
-        expectedArrivalTime:
-          appt.expectedArrivalTime && moment(appt.expectedArrivalTime).local().format('YYYY-MM-DD HH:mm'),
-        age: moment().diff(appt.patient.birth, 'years') + 'Y',
-        type: appt.registration.type,
-        doctor: appt.doctor.user.firstName,
-        subject: renderSubject(appt.subject, appt.note),
-      };
-    });
   };
 
   const onDatePickerChange = date => {
@@ -322,7 +335,8 @@ function RegistrationPage(props) {
             },
           };
         }}
-        dataSource={convertToTableSource(props.registrations)}
+        dataSource={convertToTableSource(props.registrations, selectedDoctor)}
+        scroll={{ x: 1200 }}
       />
       <RegistDrawer />
     </Container>
