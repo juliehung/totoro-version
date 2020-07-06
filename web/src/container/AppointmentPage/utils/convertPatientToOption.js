70ea88996b391e9c82f80b1474c252f2b6af350f
jinbin xu <xu@dentall.io>
2020-07-06 09:29:09 +0000

    Resolve "[1.12.1-1.0.8-hotfix-6-c27331fe] 預約卡片病歷編號需是優仕轉過來的編號"

--- web/src/container/AppointmentPage/utils/convertPatientToOption.js
+++ web/src/container/AppointmentPage/utils/convertPatientToOption.js
@@ -3,6 +3,7 @@ export default function convertPatientToOption(patients) {
     .filter(p => p.name && p.name.length > 0)
     .map(p => ({
       name: p.name,
+      medicalId: p.medicalId,
       id: p.id,
     }));
 }
