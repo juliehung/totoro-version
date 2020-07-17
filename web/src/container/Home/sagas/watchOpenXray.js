62412ed9dd794ce3a5ba4d0355f6a6f6e3ce4458
xu <xu@dentall.io>
2020-07-17 15:10:12 +0800

    add path info when calling middleman

--- web/src/container/Home/sagas/watchOpenXray.js
+++ web/src/container/Home/sagas/watchOpenXray.js
@@ -1,8 +1,11 @@
-import { take, call, put } from 'redux-saga/effects';
+import { take, call, put, select } from 'redux-saga/effects';
 import Xray from '../../../models/xray';
 import { OPEN_XRAY, XRAY_VENDORS } from '../constant';
 import { xrayGreeting, xrayGreetingSuccess, xrayGreetingFailure } from '../actions';
 import moment from 'moment';
+
+const getVixwinPath = state => state.settingPageReducer.configurations.config.vixwinPath;
+const pathValidator = path => path && path.length;
 
 export function* watchOpenXray() {
   while (true) {
@@ -32,8 +35,17 @@ function* openXray(action) {
       const patient_id = appointment.medicalId;
       yield call(Xray.get, { vendor, patient_id, patient_name, birthday, gender });
     } else if (vendor === XRAY_VENDORS.vixwin) {
+      const vixwinPath = yield select(getVixwinPath);
+      const vixwin_path = vixwinPath.application;
+      const image_path = vixwinPath.image;
       const patient_id = appointment.nationalId;
-      yield call(Xray.get, { vendor, patient_id, patient_name });
+      yield call(Xray.get, {
+        vendor,
+        patient_id,
+        patient_name,
+        ...(pathValidator(vixwin_path) && { vixwin_path }),
+        ...(pathValidator(image_path) && { image_path }),
+      });
     }
   } catch (err) {
     console.log(err);
