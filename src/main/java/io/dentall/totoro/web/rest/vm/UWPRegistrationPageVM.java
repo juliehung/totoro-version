deaf5d63818fb3ce386db3516503e2212a884b4f
weichen <weichen@dentall.io>
2020-07-21 11:07:51 +0800

    Add accounting.transation_time

--- src/main/java/io/dentall/totoro/web/rest/vm/UWPRegistrationPageVM.java
+++ src/main/java/io/dentall/totoro/web/rest/vm/UWPRegistrationPageVM.java
@@ -43,6 +43,9 @@ public interface UWPRegistrationPageVM {
     String getNhiExtendDisposalA23();
     String getNhiExtendDisposalA54();
 
+    // accounting
+    Instant getTransactionTime();
+
     // other needed
     String getPatientTags();
     Long getProcedureCounter();
