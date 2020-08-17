dc648e6d65ab40c5b3174ece13cff82c60ec76a6
xu <xu@dentall.io>
2020-08-17 17:15:17 +0800

    Remove legacy antd form

--- web/src/container/LoginPage/LoginForm.js
+++ web/src/container/LoginPage/LoginForm.js
@@ -1,39 +1,62 @@
 import React from 'react';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Form } from '@ant-design/compatible';
-import '@ant-design/compatible/assets/index.css';
-import { Input, Button } from 'antd';
+import { Input, Button, Form } from 'antd';
 import { login } from './actions';
 import { connect } from 'react-redux';
 
-class NormalLoginForm extends React.Component {
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        this.props.login(values);
-      }
-    });
+const layout = {
+  labelCol: {
+    span: 0,
+  },
+  wrapperCol: {
+    span: 24,
+  },
+};
+
+class LoginForm extends React.Component {
+  onFinish = values => {
+    this.props.login(values);
+  };
+
+  onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
   };
 
   render() {
-    const { getFieldDecorator } = this.props.form;
     return (
-      <Form onSubmit={this.handleSubmit} className="login-form">
-        <Form.Item>
-          {getFieldDecorator('username', {
-            rules: [{ required: true, message: 'Please input your username!' }],
-          })(<Input prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="帳號" />)}
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={this.onFinish}
+        onFinishFailed={this.onFinishFailed}
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: '帳號為必填!',
+            },
+          ]}
+        >
+          <Input prefix={<UserOutlined style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="帳號" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: '密碼為必填!',
+            },
+          ]}
+        >
+          <Input.Password prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="密碼" />
         </Form.Item>
         <Form.Item>
-          {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
-          })(
-            <Input prefix={<LockOutlined style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="密碼" />,
-          )}
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit" className="login-form-button" block>
+          <Button type="primary" htmlType="submit" block>
             登入
           </Button>
         </Form.Item>
@@ -41,8 +64,6 @@ class NormalLoginForm extends React.Component {
     );
   }
 }
-
-const LoginForm = Form.create({ name: 'normal_login' })(NormalLoginForm);
 
 const mapStateToProps = () => ({});
 
