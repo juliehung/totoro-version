1860a293de8c59b8b1a310c2928badd93c053dbf
WeiChen <weichen@dentall.io>
2020-09-14 12:47:10 +0800

    Add validation title for validate history

--- src/main/java/io/dentall/totoro/business/service/nhi/NhiRuleCheckResultDTO.java
+++ src/main/java/io/dentall/totoro/business/service/nhi/NhiRuleCheckResultDTO.java
@@ -4,7 +4,14 @@ public class NhiRuleCheckResultDTO {
 
     private boolean validated;
 
+    private String validateTitle;
+
     private String message;
+
+    public NhiRuleCheckResultDTO validateTitle(String validateTitle) {
+        this.validateTitle = validateTitle;
+        return this;
+    }
 
     public NhiRuleCheckResultDTO validated(boolean validated) {
         this.validated = validated;
@@ -14,6 +21,14 @@ public class NhiRuleCheckResultDTO {
     public NhiRuleCheckResultDTO message(String message) {
         this.message = message;
         return this;
+    }
+
+    public String getValidateTitle() {
+        return validateTitle;
+    }
+
+    public void setValidateTitle(String validateTitle) {
+        this.validateTitle = validateTitle;
     }
 
     public boolean isValidated() {
