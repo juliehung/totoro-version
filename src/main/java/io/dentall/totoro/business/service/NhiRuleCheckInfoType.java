4f6e501c40564bacfd6b2d9a426a19e8b913f091
WeiChen <weichen@dentall.io>
2020-10-20 16:34:52 +0800

    Add source, and info type

---
+++ src/main/java/io/dentall/totoro/business/service/NhiRuleCheckInfoType.java
@@ -0,0 +1,5 @@
+package io.dentall.totoro.business.service;
+
+public enum NhiRuleCheckInfoType {
+    NHI_CARD_RECORD, SYSTEM_RECORD
+}
--- src/main/java/io/dentall/totoro/business/service/nhi/NhiRuleCheckResultDTO.java
+++ src/main/java/io/dentall/totoro/business/service/nhi/NhiRuleCheckResultDTO.java
@@ -1,12 +1,32 @@
 package io.dentall.totoro.business.service.nhi;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dentall.totoro.business.service.NhiRuleCheckInfoType;
+import io.dentall.totoro.business.service.NhiRuleCheckSourceType;
+
 public class NhiRuleCheckResultDTO {
 
     private boolean validated;
 
+    @JsonProperty("infoType")
+    private NhiRuleCheckInfoType nhiRuleCheckInfoType;
+
+    @JsonProperty("sourceType")
+    private NhiRuleCheckSourceType nhiRuleCheckSourceType;
+
     private String validateTitle;
 
     private String message;
+
+    public NhiRuleCheckResultDTO nhiRuleCheckInfoType(NhiRuleCheckInfoType nhiRuleCheckInfoType) {
+        this.nhiRuleCheckInfoType = nhiRuleCheckInfoType;
+        return this;
+    }
+
+    public NhiRuleCheckResultDTO nhiRuleCheckSourceType(NhiRuleCheckSourceType nhiRuleCheckSourceType) {
+        this.nhiRuleCheckSourceType = nhiRuleCheckSourceType;
+        return this;
+    }
 
     public NhiRuleCheckResultDTO validateTitle(String validateTitle) {
         this.validateTitle = validateTitle;
@@ -21,6 +41,22 @@ public class NhiRuleCheckResultDTO {
     public NhiRuleCheckResultDTO message(String message) {
         this.message = message;
         return this;
+    }
+
+    public NhiRuleCheckInfoType getNhiRuleCheckInfoType() {
+        return nhiRuleCheckInfoType;
+    }
+
+    public void setNhiRuleCheckInfoType(NhiRuleCheckInfoType nhiRuleCheckInfoType) {
+        this.nhiRuleCheckInfoType = nhiRuleCheckInfoType;
+    }
+
+    public NhiRuleCheckSourceType getNhiRuleCheckSourceType() {
+        return nhiRuleCheckSourceType;
+    }
+
+    public void setNhiRuleCheckSourceType(NhiRuleCheckSourceType nhiRuleCheckSourceType) {
+        this.nhiRuleCheckSourceType = nhiRuleCheckSourceType;
     }
 
     public String getValidateTitle() {
