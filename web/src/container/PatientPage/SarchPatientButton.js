b6c7e8d47b85802dd079842b00dd7fe11fc02c35
jinbin xu <xu@dentall.io>
2020-10-22 08:46:19 +0000

    Change patient page fab style

--- web/src/container/PatientPage/SarchPatientButton.js
+++ web/src/container/PatientPage/SarchPatientButton.js
@@ -5,23 +5,22 @@ import search from '../../images/search.svg';
 import { changeDrawerVisible } from './actions';
 
 //#region
-
 const Container = styled.div`
-  width: 50px;
-  height: 50px;
+  width: 56px;
+  height: 56px;
   border-radius: 50%;
   background-color: #3266ff;
   position: fixed;
   right: 20px;
-  bottom: 20px;
+  bottom: 21px;
   z-index: 100000;
   z-index: 400;
   display: flex;
   justify-content: center;
   align-items: center;
   cursor: pointer;
+  box-shadow: 0 8px 16px -4px rgba(50, 102, 255, 0.4);
 `;
-
 //#endregion
 
 function SarchPatientButton(props) {
