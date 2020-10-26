a8fc709e0b7369b1371d896da50b3f8b3d51e9b0
jinbin xu <xu@dentall.io>
2020-10-26 08:41:23 +0000

    Cu 4vthxd xu

--- web/src/models/treatmentProcedure.js
+++ web/src/models/treatmentProcedure.js
@@ -1,12 +1,14 @@
 import request from '../utils/request';
 import apiUrl from '../utils/apiUrl';
+import combineUrlAndQueryData from '../utils/combineUrlAndQueryData';
 
 const LOCATION = `treatment-procedures`;
 const requestUrl = `${apiUrl}/${LOCATION}`;
 
 export default class TreatmentProcedure {
-  static getRecently = async patientId => {
-    const requestURL = `${requestUrl}/by/${patientId}/recently`;
+  static getRecently = async (patientId, params) => {
+    let requestURL = `${requestUrl}/by/${patientId}/recently`;
+    requestURL = combineUrlAndQueryData(requestURL, params);
     const result = await request(requestURL);
     return result;
   };
