bb8993b4df100f01e650a5a2887112d74e3d6d60
JimCat <jim19901103@gmail.com>
2020-12-21 12:10:26 +0800

    fix sort and empty data

--- web/src/container/PatientPage/utils/convertMedicalRecordsToTableObject.js
+++ web/src/container/PatientPage/utils/convertMedicalRecordsToTableObject.js
@@ -1,10 +1,15 @@
 export default function convertMedicalRecordsToTableObject(medicalRecords) {
   if (!medicalRecords) return [];
-  return medicalRecords.map(t => {
-    const key = t.id;
+  return medicalRecords
+    .map(t => {
+      if (t) {
+        const key = t.id;
 
-    const { date, seqNumber, medicalCategory, medicalInstitutionCode } = t;
+        const { date, seqNumber, medicalCategory, medicalInstitutionCode } = t;
 
-    return { key, date, medicalCategory, medicalInstitutionCode, seqNumber };
-  });
+        return { key, date, medicalCategory, medicalInstitutionCode, seqNumber };
+      }
+    })
+    .filter(d => d?.date && d?.medicalInstitutionCode)
+    .sort((a, b) => b.date - a.date);
 }
