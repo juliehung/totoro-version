88c78acee4aff354a5cefea70ac34c1f956c34d4
JimCat <jim19901103@gmail.com>
2020-12-21 16:28:52 +0800

    fix lint problem

--- web/src/container/PatientPage/utils/convertMedicalRecordsToTableObject.js
+++ web/src/container/PatientPage/utils/convertMedicalRecordsToTableObject.js
@@ -1,16 +1,12 @@
 export default function convertMedicalRecordsToTableObject(medicalRecords) {
   if (!medicalRecords) return [];
-  const reMedicalRecords = medicalRecords
-    .map(t => {
-      if (t) {
-        const key = t.id;
+  const reMedicalRecords = medicalRecords.map(t => {
+    const key = t.id;
 
-        const { date, seqNumber, medicalCategory, medicalInstitutionCode } = t;
+    const { date, seqNumber, medicalCategory, medicalInstitutionCode } = t;
 
-        return { key, date, medicalCategory, medicalInstitutionCode, seqNumber };
-      }
-    })
-    .filter(d => d?.date && d?.medicalInstitutionCode)
-    .sort((a, b) => b.date - a.date);
-  return reMedicalRecords;
+    return { key, date, medicalCategory, medicalInstitutionCode, seqNumber };
+  });
+
+  return reMedicalRecords.filter(d => d?.date && d?.medicalInstitutionCode).sort((a, b) => b.date - a.date);
 }
