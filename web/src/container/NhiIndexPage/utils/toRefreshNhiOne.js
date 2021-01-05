3b16d3d836199a2604e33523e55e6d2689c08dac
JimCat <jim19901103@gmail.com>
2021-01-05 14:36:13 +0800

    fix number not have total value. (workaround ver.)

--- web/src/container/NhiIndexPage/utils/toRefreshNhiOne.js
+++ web/src/container/NhiIndexPage/utils/toRefreshNhiOne.js
@@ -5,7 +5,8 @@ function toRefreshValidNhiData(nhiOne, validNhiData, doctorData) {
   if (!validNhiData && !nhiOne) {
     return undefined;
   }
-  const nhiOneTableData = [];
+  const treatmentData = [];
+  const examinationData = [];
   const userNotionalIds = doctorData.map(doctor => {
     return {
       nationalId: doctor?.extendUser?.nationalId,
@@ -16,14 +17,14 @@ function toRefreshValidNhiData(nhiOne, validNhiData, doctorData) {
   if (nhiOne && nhiOne?.treatmentProcedures.length !== 0) {
     for (const treatmentProcedure of nhiOne?.treatmentProcedures) {
       const nhiName = `${treatmentProcedure?.nhiProcedure?.code} ${treatmentProcedure?.nhiProcedure?.name}`;
-      nhiOneTableData.push({
+      treatmentData.push({
         _id: `${nhiOne?.id}-${uuid()}`,
         nhiName,
         point: treatmentProcedure?.nhiProcedure?.point,
         total: treatmentProcedure?.total,
         quantity: treatmentProcedure?.quantity,
         multiplier:
-          !!treatmentProcedure?.total &&
+          treatmentProcedure?.total &&
           `${
             Math.round(
               treatmentProcedure?.total / (treatmentProcedure?.quantity * treatmentProcedure?.nhiProcedure?.point),
@@ -37,7 +38,7 @@ function toRefreshValidNhiData(nhiOne, validNhiData, doctorData) {
       const nhiName = `${nhiExtendDisposal?.examinationCode} ${
         staticExaminationData[nhiExtendDisposal?.examinationCode]
       }`;
-      nhiOneTableData.push({
+      examinationData.push({
         _id: `${nhiOne?.id}-${uuid()}`,
         nhiName,
         point: nhiExtendDisposal?.examinationPoint,
@@ -51,18 +52,17 @@ function toRefreshValidNhiData(nhiOne, validNhiData, doctorData) {
   if (findMappingData.length !== 0) {
     const { nhiExtendDisposalList } = findMappingData[0];
     const findMappingDoctor = userNotionalIds.filter(({ nationalId }) => nationalId === nhiExtendDisposalList[0]?.a15);
-    const examinationPoint = nhiExtendDisposalList.length !== 0 ? nhiExtendDisposalList[0].examinationPoint : 0;
-    const total = nhiOneTableData.map(({ total }) => total).reduce((a, b) => !!a && !!b && a + b, 0);
-
+    const examinationPoint = examinationData.map(({ total }) => total).reduce((a, b) => (b ? a + b : a), 0);
+    const treatmentPoint = treatmentData.map(({ total }) => total).reduce((a, b) => (b ? a + b : a), 0);
     return {
       ...nhiOne,
       patientName: nhiExtendDisposalList[0]?.patientName,
       doctorName: findMappingDoctor.length !== 0 ? findMappingDoctor[0].doctorName : '',
       patientNationalId: nhiExtendDisposalList[0]?.a12,
       examinationPoint,
-      treatmentPoint: nhiOneTableData.map(({ total }) => total).reduce((a, b) => !!a && !!b && a + b, 0),
-      totalPoint: total + examinationPoint,
-      nhiOneTableData,
+      treatmentPoint,
+      totalPoint: examinationPoint + treatmentPoint,
+      nhiOneTableData: [].concat(treatmentData, examinationData),
     };
   }
   return nhiOne;
