4535ff385c073e329968922c0078c93ab0c2bfec
JimCat <jim19901103@gmail.com>
2021-01-11 11:18:30 +0800

    fix week text wrong display with cross year

--- web/src/container/ShiftPage/utils/parseDateToString.js
+++ web/src/container/ShiftPage/utils/parseDateToString.js
@@ -2,10 +2,19 @@ import moment from 'moment';
 
 export function parseDateToString(date) {
   const momentDate = moment(date);
+  const moment7Date = moment(date).add(6, 'day');
   const year = momentDate.year() - 1911;
-  return year + '年' + moment(date).format(' 第 w 週, MMM') + '第' + weekOfMonth(moment(date)) + '週';
+  const moment7Year = moment7Date.year() - 1911;
+  const currentYearWeek =
+    moment7Year !== year ? parseInt(moment(date).subtract(6, 'day').format('w')) + 1 : momentDate.format('w');
+  return `${year}年 第 ${currentYearWeek} 週, ${moment(date).format('M')}月第 ${weekOfMonth(
+    moment7Year !== year,
+    momentDate,
+  )} 週`;
 }
 
-function weekOfMonth(m) {
-  return m.week() - moment(m).startOf('month').week() + 1;
+function weekOfMonth(isNextYear, m) {
+  return isNextYear
+    ? m.subtract(6, 'day').week() - moment(m).startOf('month').week() + 2
+    : m.week() - moment(m).startOf('month').week() + 1;
 }
