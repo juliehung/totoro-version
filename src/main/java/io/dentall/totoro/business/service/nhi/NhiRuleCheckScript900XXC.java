61211a60e442f698f55104e87e5b0c941429e442
weichen <weichen@dentall.io>
2021-04-15 16:46:41 +0800

    Impl 90012C

---
+++ src/main/java/io/dentall/totoro/business/service/nhi/NhiRuleCheckScript900XXC.java
@@ -0,0 +1,67 @@
+package io.dentall.totoro.business.service.nhi;
+
+import io.dentall.totoro.business.service.nhi.util.NhiRuleCheckFormat;
+import io.dentall.totoro.business.service.nhi.util.NhiRuleCheckUtil;
+import io.dentall.totoro.business.service.nhi.util.ToothConstraint;
+import io.dentall.totoro.business.vm.nhi.NhiRuleCheckResultVM;
+
+import java.util.Arrays;
+
+public class NhiRuleCheckScript900XXC {
+
+    private final NhiRuleCheckUtil nhiRuleCheckUtil;
+
+    public NhiRuleCheckScript900XXC(
+        NhiRuleCheckUtil nhiRuleCheckUtil
+    ) {
+        this.nhiRuleCheckUtil = nhiRuleCheckUtil;
+    }
+
+
+    public NhiRuleCheckResultVM validate89001C(NhiRuleCheckDTO dto) {
+        NhiRuleCheckResultVM vm = new NhiRuleCheckResultVM();
+
+        nhiRuleCheckUtil.addResultToVm(
+            nhiRuleCheckUtil.addNotification(
+                String.format(
+                    NhiRuleCheckFormat.XRAY.getFormat(),
+                    dto.getNhiExtendTreatmentProcedure().getA73()
+                )
+            ),
+            vm
+        );
+
+        if (vm.isValidated()) {
+            nhiRuleCheckUtil.addResultToVm(
+                nhiRuleCheckUtil.isNoTreatmentAtSpecificTooth(
+                    dto,
+                    Arrays.asList("92013C~92015C")
+                ),
+                vm
+            );
+        }
+
+        if (vm.isValidated()) {
+            nhiRuleCheckUtil.addResultToVm(
+                nhiRuleCheckUtil.isNoNhiMedicalRecordAtSpecificTooth(
+                    dto,
+                    Arrays.asList("92013C~92015C")
+                ),
+                vm
+            );
+        }
+
+        if (vm.isValidated()) {
+            nhiRuleCheckUtil.addResultToVm(
+                nhiRuleCheckUtil.isAllLimitedTooth(
+                    dto,
+                    ToothConstraint.PERMANENT_TOOTH
+                ),
+                vm
+            );
+        }
+
+        return vm;
+    }
+
+}
