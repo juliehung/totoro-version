f638e5bcbe9bd9a8da8d11aaa1643afd8ce78f15
weichen <weichen@dentall.io>
2021-09-02 11:57:16 +0800

    Fix security util can not be used in fork pool

--- src/main/java/io/dentall/totoro/web/rest/NhiMetricResource.java
+++ src/main/java/io/dentall/totoro/web/rest/NhiMetricResource.java
@@ -80,132 +80,6 @@ public class NhiMetricResource {
         return ResponseEntity.ok().body(map);
     }
 
-
-    @GetMapping("/taipei-district")
-    @Timed
-    public ResponseEntity<Map<String, List<TaipeiDistrictDto>>> getTaipeiDistrictMetric(
-        @RequestParam LocalDate begin,
-        @RequestParam(required = false) List<Long> excludeDisposalId,
-        @RequestParam(required = false) List<Long> doctorIds) {
-        log.debug("REST request to taipei district : begin={}, excludeDisposalId={}, doctorIds={}", begin, excludeDisposalId, doctorIds);
-
-        List<TaipeiDistrictDto> vm = metricService.getTaipeiDistrictMetric(begin, excludeDisposalId, doctorIds);
-        Map<String, List<TaipeiDistrictDto>> map = new HashMap<>();
-        map.put("metrics", vm);
-
-        return ResponseEntity.ok().body(map);
-    }
-
-    @GetMapping("/north-district")
-    @Timed
-    public ResponseEntity<Map<String, List<NorthDistrictDto>>> getNorthDistrictMetric(
-        @RequestParam LocalDate begin,
-        @RequestParam(required = false) List<Long> excludeDisposalId,
-        @RequestParam(required = false) List<Long> doctorIds) {
-        log.debug("REST request to north district : begin={}, excludeDisposalId={}, doctorIds={}", begin, excludeDisposalId, doctorIds);
-
-        List<NorthDistrictDto> vm = metricService.getNorthDistrictMetric(begin, excludeDisposalId, doctorIds);
-        Map<String, List<NorthDistrictDto>> map = new HashMap<>();
-        map.put("metrics", vm);
-
-        return ResponseEntity.ok().body(map);
-    }
-
-    @GetMapping("/middle-district")
-    @Timed
-    public ResponseEntity<Map<String, List<MiddleDistrictDto>>> getMiddleDistrictMetric(
-        @RequestParam LocalDate begin,
-        @RequestParam(required = false) List<Long> excludeDisposalId,
-        @RequestParam(required = false) List<Long> doctorIds) {
-        log.debug("REST request to middle district : begin={}, excludeDisposalId={}, doctorIds={}", begin, excludeDisposalId, doctorIds);
-
-        List<MiddleDistrictDto> vm = metricService.getMiddleDistrictMetric(begin, excludeDisposalId, doctorIds);
-        Map<String, List<MiddleDistrictDto>> map = new HashMap<>();
-        map.put("metrics", vm);
-
-        return ResponseEntity.ok().body(map);
-    }
-
-    @GetMapping("/south-district")
-    @Timed
-    public ResponseEntity<Map<String, List<SouthDistrictDto>>> getSouthDistrictMetric(
-        @RequestParam LocalDate begin,
-        @RequestParam(required = false) List<Long> excludeDisposalId,
-        @RequestParam(required = false) List<Long> doctorIds) {
-        log.debug("REST request to south district : begin={}, excludeDisposalId={}, doctorIds={}", begin, excludeDisposalId, doctorIds);
-
-        List<SouthDistrictDto> vm = metricService.getSouthDistrictMetric(begin, excludeDisposalId, doctorIds);
-        Map<String, List<SouthDistrictDto>> map = new HashMap<>();
-        map.put("metrics", vm);
-
-        return ResponseEntity.ok().body(map);
-    }
-
-    @GetMapping("/east-district")
-    @Timed
-    public ResponseEntity<Map<String, List<EastDistrictDto>>> getEastDistrictMetric(
-        @RequestParam LocalDate begin,
-        @RequestParam(required = false) List<Long> excludeDisposalId,
-        @RequestParam(required = false) List<Long> doctorIds) {
-        log.debug("REST request to east district : begin={}, excludeDisposalId={}, doctorIds={}", begin, excludeDisposalId, doctorIds);
-
-        List<EastDistrictDto> vm = metricService.getEastDistrictMetric(begin, excludeDisposalId, doctorIds);
-        Map<String, List<EastDistrictDto>> map = new HashMap<>();
-        map.put("metrics", vm);
-
-        return ResponseEntity.ok().body(map);
-    }
-
-    @GetMapping("/kao-ping-district-regular")
-    @Timed
-    public ResponseEntity<Map<String, List<KaoPingDistrictRegularDto>>> getKaoPingDistrictRegularMetric(
-        @RequestParam LocalDate begin,
-        @RequestParam(required = false) List<Long> excludeDisposalId,
-        @RequestParam(required = false) List<Long> doctorIds) {
-        log.debug("REST request to kao-ping district regular : begin={}, excludeDisposalId={}, doctorIds={}", begin, excludeDisposalId, doctorIds);
-
-        List<KaoPingDistrictRegularDto> vm = metricService.getKaoPingDistrictRegularMetric(begin, excludeDisposalId, doctorIds);
-        Map<String, List<KaoPingDistrictRegularDto>> map = new HashMap<>();
-        map.put("metrics", vm);
-
-        return ResponseEntity.ok().body(map);
-    }
-
-    @GetMapping("/kao-ping-district-reduction")
-    @Timed
-    public ResponseEntity<Map<String, List<KaoPingDistrictReductionDto>>> getKaoPingDistrictReductionMetric(
-        @RequestParam LocalDate begin,
-        @RequestParam(required = false) List<Long> excludeDisposalId,
-        @RequestParam(required = false) List<Long> doctorIds) {
-        log.debug("REST request to kao-ping district reduction : begin={}, excludeDisposalId={}, doctorIds={}", begin, excludeDisposalId, doctorIds);
-
-        List<KaoPingDistrictReductionDto> vm = metricService.getKaoPingDistrictReductionMetric(begin, excludeDisposalId, doctorIds);
-        Map<String, List<KaoPingDistrictReductionDto>> map = new HashMap<>();
-        map.put("metrics", vm);
-
-        return ResponseEntity.ok().body(map);
-    }
-
-    @GetMapping("/composite-district")
-    @Timed
-    public ResponseEntity<CompositeDistrictDto> getCompositeDistrictReductionMetric(
-        @RequestParam LocalDate begin,
-        @RequestParam(required = false) List<Long> excludeDisposalId,
-        @RequestParam(required = false) List<Long> doctorIds) {
-        log.debug("REST request to composite district : begin={}, excludeDisposalId={}, doctorIds={}", begin, excludeDisposalId, doctorIds);
-
-        CompositeDistrictDto dto = metricService.getCompositeDistrictMetric(begin, excludeDisposalId, doctorIds,
-            Arrays.asList(TaipeiDistrictService.class,
-                NorthDistrictService.class,
-                MiddleDistrictService.class,
-                SouthDistrictService.class,
-                EastDistrictService.class,
-                KaoPingDistrictRegularService.class,
-                KaoPingDistrictReductionService.class));
-
-        return ResponseEntity.ok().body(dto);
-    }
-
     @PostMapping("/report")
     @Timed
     @Transactional
@@ -276,7 +150,8 @@ public class NhiMetricResource {
                 nhiMetricReportBodyVM.getBegin(),
                 nhiMetricReportBodyVM.getExcludeDisposalId(),
                 nhiMetricReportBodyVM.getDoctorIds(),
-                new ArrayList<>(reportTypeServiceList)
+                new ArrayList<>(reportTypeServiceList),
+                currentUser.get()
             );
 
             NhiMetricReport r = nhiMetricReportRepository.findById(finalRecordId)
