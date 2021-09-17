4ae6199a8e277960eb12b1c72e2192f1c4c545a6
weichen <weichen@dentall.io>
2021-09-17 11:49:46 +0800

    Impl deleted tp will add to tpd

--- src/main/java/io/dentall/totoro/service/mapper/TreatmentProcedureDelMapper.java
+++ src/main/java/io/dentall/totoro/service/mapper/TreatmentProcedureDelMapper.java
@@ -2,8 +2,8 @@ package io.dentall.totoro.service.mapper;
 
 import io.dentall.totoro.domain.TreatmentProcedure;
 import io.dentall.totoro.domain.TreatmentProcedureDel;
-import io.dentall.totoro.repository.TreatmentProcedureDelRepository;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
@@ -12,6 +12,11 @@ public interface TreatmentProcedureDelMapper {
 
     TreatmentProcedureDelMapper INSTANCE = Mappers.getMapper( TreatmentProcedureDelMapper.class );
 
+    @Mapping(source = "disposal.id", target = "disposalId")
+    @Mapping(source = "appointment.id", target = "appointmentId")
+    @Mapping(source = "treatmentTask.id", target = "treatmentTaskId")
+    @Mapping(source = "procedure.id", target = "procedureId")
+    @Mapping(source = "nhiProcedure.id", target = "nhiProcedureId")
     void transformTreatmentProcedureToTreatmentProcedureDel(TreatmentProcedure treatmentProcedure, @MappingTarget TreatmentProcedureDel treatmentProcedureDel);
 
     TreatmentProcedure convertTreatmentProcedureDelToTreatmentProcedure(TreatmentProcedureDel treatmentProcedureDel);
