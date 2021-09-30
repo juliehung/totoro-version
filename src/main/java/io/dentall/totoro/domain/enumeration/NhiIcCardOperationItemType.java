0a4073140a182f0af02d5b02693c8d12ab786699
weichen <weichen@dentall.io>
2021-09-30 10:46:50 +0800

    Add new ic operation type

--- src/main/java/io/dentall/totoro/domain/enumeration/NhiIcCardOperationItemType.java
+++ src/main/java/io/dentall/totoro/domain/enumeration/NhiIcCardOperationItemType.java
@@ -3,4 +3,6 @@ package io.dentall.totoro.domain.enumeration;
 public enum NhiIcCardOperationItemType {
     TREATMENT_PROCEDURE,
     TREATMENT_DRUG,
+    // 由於健保卡會獨立寫入，診察代碼與費用，但是系統本身沒有這個結構，所以使用之後 item id 此時的資料等同於空值 0
+    EXAMINATION,
 }
