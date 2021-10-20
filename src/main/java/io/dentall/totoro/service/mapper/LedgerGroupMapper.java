ac0095714b24715ad0aa3f73aa7234eae8447106
weichen <weichen@dentall.io>
2021-10-20 16:50:46 +0800

    Support ledger group

---
+++ src/main/java/io/dentall/totoro/service/mapper/LedgerGroupMapper.java
@@ -0,0 +1,18 @@
+package io.dentall.totoro.service.mapper;
+
+import io.dentall.totoro.domain.LedgerGroup;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.factory.Mappers;
+
+@Mapper( nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE )
+public interface LedgerGroupMapper {
+
+    LedgerGroupMapper INSTANCE = Mappers.getMapper( LedgerGroupMapper.class );
+
+    void patching(
+        @MappingTarget LedgerGroup origin,
+        LedgerGroup patch
+    );
+}
