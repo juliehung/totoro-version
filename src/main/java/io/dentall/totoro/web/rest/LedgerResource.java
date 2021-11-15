e64759b83e5455401d3cc11525a60ff33655ca22
weichen <weichen@dentall.io>
2021-11-15 10:25:56 +0800

    Add profile for leddger resource

--- src/main/java/io/dentall/totoro/web/rest/LedgerResource.java
+++ src/main/java/io/dentall/totoro/web/rest/LedgerResource.java
@@ -15,6 +15,7 @@ import io.dentall.totoro.web.rest.util.PaginationUtil;
 import io.dentall.totoro.web.rest.vm.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
@@ -34,6 +35,7 @@ import java.util.stream.Collectors;
 /**
  * REST controller for managing Ledger.
  */
+@Profile("img-gcs")
 @RestController
 @RequestMapping("/api")
 public class LedgerResource {
