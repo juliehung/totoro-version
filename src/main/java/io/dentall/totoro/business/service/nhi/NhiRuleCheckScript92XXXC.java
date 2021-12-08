4bcc6003937b378c4c724881d17f7549d96c1ff5
weichen <weichen@dentall.io>
2021-12-08 17:31:39 +0800

    Fix 92072C

--- src/main/java/io/dentall/totoro/business/service/nhi/NhiRuleCheckScript92XXXC.java
+++ src/main/java/io/dentall/totoro/business/service/nhi/NhiRuleCheckScript92XXXC.java
@@ -693,11 +693,24 @@ public class NhiRuleCheckScript92XXXC {
             nhiRuleCheckUtil.isCodeBeforeDateV2(
                 dto,
                 null,
-                Arrays.asList("92072C", "P6702C", "P6703C", "P6704C", "P6705C"),
+                Arrays.asList("92072C"),
                 nhiRuleCheckUtil.regularDayDurationCalculation(dto, DateTimeUtil.NHI_90_DAY),
                 String.valueOf(DateTimeUtil.NHI_90_DAY.getDays()),
                 1,
                 NhiRuleCheckFormat.D4_1
+            ),
+            vm
+        );
+
+        nhiRuleCheckUtil.addResultToVm(
+            nhiRuleCheckUtil.isCodeBeforeDateV2(
+                dto,
+                null,
+                Arrays.asList("P6702C", "P6703C", "P6704C", "P6705C"),
+                nhiRuleCheckUtil.regularDayDurationCalculation(dto, DateTimeUtil.NHI_90_DAY),
+                String.valueOf(DateTimeUtil.NHI_90_DAY.getDays()),
+                1,
+                NhiRuleCheckFormat.D1_2
             ),
             vm
         );
