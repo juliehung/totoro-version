4dfac6ea576f55f40c05c8a171c4d32e7876d12c
kaslrca-dentall <kaslrca@dentall.io>
2021-12-24 17:04:10 +0800

    調整欄位寬度

--- src/main/java/io/dentall/totoro/business/service/report/treatment/MonthlyDrugSheet.java
+++ src/main/java/io/dentall/totoro/business/service/report/treatment/MonthlyDrugSheet.java
@@ -10,6 +10,7 @@ import org.apache.poi.xssf.usermodel.XSSFColor;
 
 import java.util.List;
 
+import static io.dentall.totoro.business.service.report.context.ReportHelper.calculateColumnWith;
 import static io.dentall.totoro.business.service.report.context.ReportHelper.displayDisposalDate;
 
 public class MonthlyDrugSheet extends ExcelSheet {
@@ -20,32 +21,73 @@ public class MonthlyDrugSheet extends ExcelSheet {
     @Override
     public void write() {
         List<SubjectMonthlyDrugVo> data = getReport().getData();
+        Font defaultFont = getReport().getBook().getWorkbook().createFont();
+        defaultFont.setFontHeightInPoints((short) 11);
+        defaultFont.setFontName("Calibri");
+        CellStyle dataCellStyle = getReport().getBook().getWorkbook().createCellStyle();
+        dataCellStyle.setFont(defaultFont);
 
         for (SubjectMonthlyDrugVo vo : data) {
             Counter rowCounter = new Counter();
             Sheet sheet = getReport().getBook().getWorkbook().createSheet(vo.getSubjectName());
-            createHeader(sheet, rowCounter);
+            long subjectId = vo.getSubjectId();
+            createHeader(sheet, rowCounter, defaultFont, vo);
 
             vo.getSummaryList().forEach(summary -> {
                 Row row = sheet.createRow(rowCounter.get());
                 Counter cellCounter = new Counter();
-                row.createCell(cellCounter.get()).setCellValue(displayDisposalDate(summary.getDisposalMonth()));
-                row.createCell(cellCounter.get()).setCellValue(summary.getDoctorName());
-                row.createCell(cellCounter.get()).setCellValue(summary.getDrugName());
-                row.createCell(cellCounter.get()).setCellValue(summary.getDrugNhiCode());
-                row.createCell(cellCounter.get()).setCellValue(summary.getProcedureCount());
-                row.createCell(cellCounter.get()).setCellValue(summary.getPatientCount());
+                Cell cell;
+
+                cell = row.createCell(cellCounter.get());
+                cell.setCellValue(displayDisposalDate(summary.getDisposalMonth()));
+                cell.setCellStyle(dataCellStyle);
+
+                if (subjectId != Long.MIN_VALUE) {
+                    cell = row.createCell(cellCounter.get());
+                    cell.setCellValue(summary.getDoctorName());
+                    cell.setCellStyle(dataCellStyle);
+                }
+
+                cell = row.createCell(cellCounter.get());
+                cell.setCellValue(summary.getDrugName());
+                cell.setCellStyle(dataCellStyle);
+
+                cell = row.createCell(cellCounter.get());
+                cell.setCellValue(summary.getDrugNhiCode());
+                cell.setCellStyle(dataCellStyle);
+
+                cell = row.createCell(cellCounter.get());
+                cell.setCellValue(summary.getProcedureCount());
+                cell.setCellStyle(dataCellStyle);
+
+                cell = row.createCell(cellCounter.get());
+                cell.setCellValue(summary.getPatientCount());
+                cell.setCellStyle(dataCellStyle);
             });
         }
     }
 
-    private void createHeader(Sheet sheet, Counter rowCounter) {
+    private void createHeader(Sheet sheet, Counter rowCounter, Font font, SubjectMonthlyDrugVo vo) {
+        sheet.setColumnWidth(0, calculateColumnWith(7, 8));
+        if (vo.getSubjectId() != Long.MIN_VALUE) {
+            sheet.setColumnWidth(1, calculateColumnWith(7, 12));
+            sheet.setColumnWidth(2, calculateColumnWith(7, 20));
+            sheet.setColumnWidth(3, calculateColumnWith(7, 12));
+            sheet.setColumnWidth(4, calculateColumnWith(7, 5));
+            sheet.setColumnWidth(5, calculateColumnWith(7, 12));
+        } else {
+            sheet.setColumnWidth(1, calculateColumnWith(7, 20));
+            sheet.setColumnWidth(2, calculateColumnWith(7, 12));
+            sheet.setColumnWidth(3, calculateColumnWith(7, 5));
+            sheet.setColumnWidth(4, calculateColumnWith(7, 12));
+        }
+
         Row header = sheet.createRow(rowCounter.get());
-        sheet.getWorkbook().createCellStyle();
         XSSFColor cellColor = new XSSFColor(new byte[]{(byte) 243, (byte) 243, (byte) 243}, new DefaultIndexedColorMap());
         CellStyle cellStyle = sheet.getWorkbook().createCellStyle();
         ((XSSFCellStyle) cellStyle).setFillForegroundColor(cellColor);
         cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        cellStyle.setFont(font);
 
         XSSFColor borderColor = new XSSFColor(new byte[]{(byte) 222, (byte) 223, (byte) 223}, new DefaultIndexedColorMap());
         cellStyle.setBorderTop(BorderStyle.THIN);
@@ -61,9 +103,11 @@ public class MonthlyDrugSheet extends ExcelSheet {
         Cell cell = header.createCell(cellCounter.get());
         cell.setCellValue("治療月份");
         cell.setCellStyle(cellStyle);
-        cell = header.createCell(cellCounter.get());
-        cell.setCellValue("醫師名稱");
-        cell.setCellStyle(cellStyle);
+        if (vo.getSubjectId() != Long.MIN_VALUE) {
+            cell = header.createCell(cellCounter.get());
+            cell.setCellValue("醫師名稱");
+            cell.setCellStyle(cellStyle);
+        }
         cell = header.createCell(cellCounter.get());
         cell.setCellValue("藥品");
         cell.setCellStyle(cellStyle);
