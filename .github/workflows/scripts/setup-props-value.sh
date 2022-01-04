0fc454aabc5b3ce41b14184eaa063e6bcb32b5df
weichen <weichen@dentall.io>
2022-01-04 11:19:21 +0800

    Remove fail when tag is not fit with old props

--- .github/workflows/scripts/setup-props-value.sh
+++ .github/workflows/scripts/setup-props-value.sh
@@ -36,10 +36,14 @@ elif [[ "${TagName}" =~ ^[0-9]+.[0-9]+.[0-9]+.*$ ]]; then
     GcpRelease=true
     FirebaseRelease=true
     RunNhiRuleTest=true
-else
-    exit 1
 fi
-
+# tag should be like milestone-1.33-wat_ever_you_want_except_hypen
+# e.g.
+# O - milestone-1.33-1234.12334.41
+# O - milestone-1.33-1234
+# O - milestone-1.33-abcd123.123
+# x - milestone-1.33-123-ab
+milestone=$(echo ${TagName} | awk '{split($1, a, "-"); printf "%s-%s", a[1], a[2];}')
 echo "::set-output name=tag-name::${TagName}"
 echo "::set-output name=tag-timestamp::${TagTimestamp}"
 echo "::set-output name=war-name::${WarName}"
@@ -51,3 +55,4 @@ echo "::set-output name=gcp-release::${GcpRelease}"
 echo "::set-output name=firebase-release::${FirebaseRelease}"
 echo "::set-output name=run-nhi-rule-test::${RunNhiRuleTest}"
 echo "::set-output name=bucket-name::${BucketName}"
+echo "::set-output name=milestone::${milestone}"
