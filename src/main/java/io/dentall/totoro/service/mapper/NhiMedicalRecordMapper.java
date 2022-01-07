41e0516520cc6a68b7211c8a146e5542a22aa432
weichen <weichen@dentall.io>
2022-01-07 12:25:43 +0800

    Redefine ic record query logic

---
+++ src/main/java/io/dentall/totoro/service/mapper/NhiMedicalRecordMapper.java
@@ -0,0 +1,32 @@
+package io.dentall.totoro.service.mapper;
+
+import io.dentall.totoro.domain.Authority;
+import io.dentall.totoro.domain.User;
+import io.dentall.totoro.service.dto.DoctorVM;
+import io.dentall.totoro.web.rest.vm.NhiMedicalRecordVM;
+import io.dentall.totoro.web.rest.vm.NhiMedicalRecordVM2;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.Named;
+import org.mapstruct.factory.Mappers;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Mapper
+public interface NhiMedicalRecordMapper {
+
+    NhiMedicalRecordMapper INSTANCE = Mappers.getMapper(NhiMedicalRecordMapper.class);
+
+    @Mapping(target = "nhiMedicalRecord.date", source = "date")
+    @Mapping(target = "nhiMedicalRecord.nhiCategory", source = "nhiCategory")
+    @Mapping(target = "nhiMedicalRecord.nhiCode", source = "nhiCode")
+    @Mapping(target = "nhiMedicalRecord.part", source = "part")
+    @Mapping(target = "nhiMedicalRecord.usage", source = "usage")
+    @Mapping(target = "nhiMedicalRecord.note", source = "note")
+    @Mapping(target = "nhiMedicalRecord.days", source = "days")
+    @Mapping(target = "mandarin", source = "mandarin")
+    NhiMedicalRecordVM nhiMedicalRecordVM2ToNhiMedicalRecordVM(NhiMedicalRecordVM2 nhiMedicalRecordVM2);
+
+}
---
+++ src/main/java/io/dentall/totoro/web/rest/vm/NhiMedicalRecordVM2.java
@@ -0,0 +1,26 @@
+package io.dentall.totoro.web.rest.vm;
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import io.dentall.totoro.domain.NhiMedicalRecord;
+
+import javax.persistence.Column;
+
+public interface NhiMedicalRecordVM2 {
+
+    String getDate();
+
+    String getNhiCategory();
+
+    String getNhiCode();
+
+    String getPart();
+
+    String getUsage();
+
+    String getNote();
+
+    String getDays();
+
+    String getMandarin();
+
+}
