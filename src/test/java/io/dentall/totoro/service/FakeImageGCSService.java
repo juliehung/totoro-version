bb16df792f136905784ac35941756248191b1898
kaslrca-dentall <kaslrca@dentall.io>
2022-01-10 18:29:50 +0800

    fix unit test

--- src/test/java/io/dentall/totoro/service/FakeImageGCSService.java
+++ src/test/java/io/dentall/totoro/service/FakeImageGCSService.java
@@ -1,5 +1,6 @@
 package io.dentall.totoro.service;
 
+import com.google.api.gax.paging.Page;
 import com.google.cloud.storage.Blob;
 import io.dentall.totoro.business.service.ImageGcsBusinessService;
 import io.dentall.totoro.repository.ImageRepository;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Collections;
 
 import static java.util.Optional.ofNullable;
 
@@ -39,5 +41,41 @@ public class FakeImageGCSService extends ImageGcsBusinessService {
     @Override
     public String getUrlForDownload() {
         return "http://fake.url/fakeBucket/";
+    }
+
+
+    @Override
+    public Page<Blob> listFileByPrefix(String prefix) {
+        return new Page<Blob>() {
+            @Override
+            public boolean hasNextPage() {
+                return false;
+            }
+
+            @Override
+            public String getNextPageToken() {
+                return null;
+            }
+
+            @Override
+            public Page<Blob> getNextPage() {
+                return null;
+            }
+
+            @Override
+            public Iterable<Blob> iterateAll() {
+                return Collections.emptyList();
+            }
+
+            @Override
+            public Iterable<Blob> getValues() {
+                return null;
+            }
+        };
+    }
+
+    @Override
+    public Blob getFile(String name) {
+        return null;
     }
 }
